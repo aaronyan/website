ef0139b288411c0cfbcaa317a5f3fdd70faa8c48
Aaron Yan <aaronyan815@gmail.com>
2015-03-14 19:26:59 -0500

    added legend colors with responsive x-axis movement.

--- js/skills_rightchart.js
+++ js/skills_rightchart.js
@@ -4,6 +4,7 @@ var data_right = [87.5, 75, 75, 62.5, 62.5, 50],
 	data_skill_names = ["","Rookie","Geek","Ninja","Master"],
 	data_legend = ['Back-End','Front-end'],
 	data_legend_colors = ['91e0f2','f9d595']
+	data_legend_x = [30, 60]
 	margin = {top: 30, right: 25, bottom: 30, left: 25},
 	width = parseInt(d3.select('#rightgraph').style('width'), 10),
 	width = width - margin.left - margin.right,
@@ -64,7 +65,7 @@ canvas.append('g')
 canvas.append('g').append("path")
 	.attr("class", "line");
 
-var bars = canvas.append('g').selectAll("rect")
+var bars = canvas.append('g').selectAll(".bar")
 	.data(data_right)
 	.enter()
 		.append("rect")
@@ -72,7 +73,8 @@ var bars = canvas.append('g').selectAll("rect")
 		.attr("height", 40)
 		.attr("y", function(d, i){ return i*barHeight+20;})
 		.attr("x", 1)
-		.attr('fill', function(d, i){ return "#"+data_right_colors[i];});
+		.attr('fill', function(d, i){ return "#"+data_right_colors[i];})
+		.attr("class", "bar");
 
 canvas.append('g').selectAll('text')
 	.data(data_right_name)
@@ -82,16 +84,16 @@ canvas.append('g').selectAll('text')
 	.attr("x", 15)
 	.attr("y", function(d, i){ return (i-5)*barHeight+46;});
 
-var key1 = canvas.append('g').append("rect")
-	.data(data_legend)
+var key1 = canvas.append('g').selectAll(".legend")
+	.data(data_legend_x)
 	.enter()
 		.append("rect")
 		.attr("width", 30)
 		.attr("height", 30)
-		.attr("y", 360)
-		.attr("x", function(d, i){ return i*100;})
-		.attr('fill', function(d, i){ return "#"+data_legend_colors[i];});
-
+		.attr("y", 380)
+		.attr("x", function(d){ return widthScale(d);})
+		.attr('fill', function(d, i){ return "#"+data_legend_colors[i];})
+		.attr("class", "legend");
 
 d3.select(window).on('resize', resize); 
  
@@ -105,8 +107,11 @@ function resize() {
     d3.select(canvas.node().parentNode)
         .style('width', (width + margin.left + margin.right) + 'px');
 
-    canvas.selectAll('rect')
+    canvas.selectAll(".bar")
         .attr("width", function(d){ return widthScale(d);});
+
+    canvas.selectAll(".legend")
+        .attr("x", function(d){ return widthScale(d);});
 
     axisName.rangePoints([0,width]);
     canvas.select('.x.axis.top')
@@ -117,7 +122,7 @@ function resize() {
         .style("text-anchor", "end");
 
     canvas.select('.grid')
-    .attr("transform", "translate(0," + 380 + ")")
+    .attr("transform", "translate(0," + 375 + ")")
     .call(x2Axis.orient('bottom'))
 
 }
