4c03c8f243712841b7a19d64f29d56494822759f
Aaron Yan <aaronyan815@gmail.com>
2015-03-15 20:48:00 -0500

    added legend and finalized comments.

--- js/skills_rightchart.js
+++ js/skills_rightchart.js
@@ -1,16 +1,20 @@
+// INITIAL VARIABLES
+
 var data_right = [87.5, 75, 75, 62.5, 62.5, 50],
 	data_right_name = ['','','','','','Python','Mongo','MySQL','HTML & CSS','JavaScript','Hive'],
 	data_right_colors = ['91e0f2','91e0f2','91e0f2','f9d595','f9d595','91e0f2'],
 	data_skill_names = ["","Rookie","Geek","Ninja","Master"],
-	data_legend = ['Back-End','Front-end'],
+	data_legend = ['','','','','','','','','','','','Back-End','Front-end'],
 	data_legend_colors = ['91e0f2','f9d595']
-	data_legend_x = [30, 60]
+	data_legend_x = [20, 60]
 	margin = {top: 30, right: 25, bottom: 30, left: 25},
 	width = parseInt(d3.select('#rightgraph').style('width'), 10),
 	width = width - margin.left - margin.right,
-	height = 450, // placeholder
+	height = 480, // placeholder
 	barHeight = 60,
 	spacing = 3;
+
+// SCALES
 
 var widthScale = d3.scale.linear()
 					.domain([0,100])
@@ -19,6 +23,10 @@ var widthScale = d3.scale.linear()
 var axisName = d3.scale.ordinal()
 				.domain(data_skill_names)
 				.rangePoints([0,width]);
+
+var y = d3.scale.linear().range([375, 0]);
+
+// AXES
 
 var xAxis = d3.svg.axis()
 			.scale(axisName)
@@ -30,13 +38,13 @@ var x2Axis = d3.svg.axis()
 	.tickSize(-420)
 	.tickValues([25,50,75,100]);
 
-var y = d3.scale.linear().range([375, 0]);
-
 var yAxis = d3.svg.axis()
 	.scale(y)
 	.orient("left")
 	.ticks(0)
 	.tickSize(0);
+
+// CANVAS & DRAWING AXES
 
 var canvas = d3.select("#rightgraph").append("svg")
 	.attr("width", (width + margin.left + margin.right) + 'px')
@@ -62,8 +70,7 @@ canvas.append('g')
 	.call(yAxis.orient('left'))
 	.attr("x", 10);
 
-canvas.append('g').append("path")
-	.attr("class", "line");
+// BARS AND BAR TEXT
 
 var bars = canvas.append('g').selectAll(".bar")
 	.data(data_right)
@@ -84,16 +91,29 @@ canvas.append('g').selectAll('text')
 	.attr("x", 15)
 	.attr("y", function(d, i){ return (i-5)*barHeight+46;});
 
+// LEGEND
+
 var key1 = canvas.append('g').selectAll(".legend")
 	.data(data_legend_x)
 	.enter()
 		.append("rect")
 		.attr("width", 30)
 		.attr("height", 30)
-		.attr("y", 380)
+		.attr("y", 400)
 		.attr("x", function(d){ return widthScale(d);})
 		.attr('fill', function(d, i){ return "#"+data_legend_colors[i];})
 		.attr("class", "legend");
+
+canvas.append('g').selectAll('.legendtext')
+	.data(data_legend)
+	.enter()
+	.append('text')
+	.text(function(d) { return d; })
+	.attr("x", function(d, i) { return data_legend_x[i-11]*width/100+35;})
+	.attr("y", 420)
+	.attr("class", "legendtext");
+
+// RESIZE WIDTH, BARS, LEGEND, AXES, GRID
 
 d3.select(window).on('resize', resize); 
  
@@ -102,17 +122,23 @@ function resize() {
     width = parseInt(d3.select('#rightgraph').style('width'), 10);
     width = width - margin.left - margin.right;
  
-    // resize the chart
+    // resize the canvas
     widthScale.range([0, width]);
     d3.select(canvas.node().parentNode)
         .style('width', (width + margin.left + margin.right) + 'px');
 
+    // resize the bars
     canvas.selectAll(".bar")
         .attr("width", function(d){ return widthScale(d);});
 
+    // resize the legend
     canvas.selectAll(".legend")
         .attr("x", function(d){ return widthScale(d);});
 
+    canvas.selectAll(".legendtext")
+        .attr("x", function(d, i){ return data_legend_x[i-11]*width/100+35;});
+
+    // resize the top x axis
     axisName.rangePoints([0,width]);
     canvas.select('.x.axis.top')
     	.call(xAxis.orient('top'))
@@ -121,6 +147,7 @@ function resize() {
         .attr("x", -10)
         .style("text-anchor", "end");
 
+    // resize the grid
     canvas.select('.grid')
     .attr("transform", "translate(0," + 375 + ")")
     .call(x2Axis.orient('bottom'))
